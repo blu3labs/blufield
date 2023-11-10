e3af41ee98dfbba1c15edd960e4cec7e2e87e026
batuhan soylu <bthnsoylu35@gmail.com>
2023-11-10 23:24:21 +0300

    add upload image method

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
-const { client } = require("./config/client");
-
-require("dotenv").config();
-
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const {client} = require("./config/client");
 const app = express();
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(cors());
+
+//services
+const {pushImage, createUser} = require("./services/userServices");
+
+app.post("/img", upload.single("file"), async (req, res) => {
+  const file = req.file;
+  console.log(file);
+  const response = await pushImage(file,client);
+  res.json(response);
+});
+
 const port = process.env.PORT || API_PORT;
 
 app.listen(port, () => {
--- services/userServices.js
+++ services/userServices.js
@@ -1,4 +1,94 @@
-const {client} = require('../config/client');
-const {generateString} = require('../utils/generateString');
-const {getAllSps} = require('../utils/getSps');
-const {broadcast} = require('../utils/broadcast');
+const { client } = require("../config/client");
+const { generateString } = require("../utils/generateString");
+const { getAllSps } = require("../utils/getSps");
+const { broadcast } = require("../utils/broadcast");
+const fs = require("fs");
+const { get } = require("http");
+const { getCheckSums } = require("@bnb-chain/greenfiled-file-handle");
+
+async function pushImage(file, client) {
+  try {
+    // save file use fs
+    const filedata = fs.readFileSync(file.path);
+
+    
+
+    console.log("get checksums started",filedata);
+    const { expectCheckSums, contentLength } = await getCheckSums(filedata);
+    console.log("tx started");
+    const imageTx = await client.object.createObject(
+      {
+        bucketName: "name",
+        contentLength: contentLength,
+        creator: process.env.WALLET_ADDRESS,
+        expectCheckSums: JSON.parse(expectCheckSums),
+        fileType: file.mimetype.split("/")[1],
+        objectName: file.originalname,
+        redundancyType: "REDUNDANCY_EC_TYPE",
+        visibility: "VISIBILITY_TYPE_PUBLIC_READ",
+      },
+      {
+        type: "ECDSA",
+        privateKey: "0x" + process.env.PRIVATE_KEY,
+        domain: generateString(10),
+        seed: generateString(10),
+        address: process.env.WALLET_ADDRESS,
+      }
+    );
+    const txHash = await imageTx.broadcast({
+      ...broadcast,
+    });
+    console.log("tx ended");
+    console.log("upload started");
+    const uploadRes = await client.object.uploadObject(
+      {
+        bucketName: "name",
+        objectName: file.originalname,
+        body: filedata,
+        txnHash: txHash.transactionHash,
+      },
+      {
+        type: "ECDSA",
+        privateKey: "0x" + process.env.PRIVATE_KEY,
+        domain: generateString(10),
+        seed: generateString(10),
+        address: process.env.WALLET_ADDRESS,
+      }
+    );
+
+    console.log(uploadRes, "upload ended");
+
+    const allSps = await getAllSps(client);
+    //remove file
+    fs.unlinkSync(file.path);
+    return {
+      url: allSps[0].endpoint + "/view/name/" + file.originalname,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      error: error,
+    };
+  }
+}
+
+async function createUser(
+  name,
+  address,
+  about,
+  cover_image,
+  profile_image,
+  bucket_id,
+  bucket_name,
+  createdDate,
+  accentColor,
+  socail_media,
+  montly_price
+) {
+
+}
+
+module.exports = {
+  createUser,
+  pushImage,
+};
