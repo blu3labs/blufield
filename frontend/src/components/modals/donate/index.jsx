4e951778bbd6d5dba24c22afef01da7fe5144e92
Mehmet Çığırgan <mehmet.cigirgan@ogr.deu.edu.tr>
2023-11-10 22:50:08 +0300

    donate modal user balance get

--- frontend/src/components/modals/donate/index.jsx
+++ frontend/src/components/modals/donate/index.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Modal } from "antd";
+import { getAddress } from "@/utils/getAddress";
+import { getNativeBalance } from "@/utils/getNativeBalance";
+import { useAccount } from "wagmi";
+import BigNumer from "bignumber.js";
 import "./index.css";
 
 function DonateModal() {
+  const { isConnected } = useAccount();
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleCancel = () => {
@@ -12,6 +18,38 @@ function DonateModal() {
   const handleShow = () => {
     setIsModalOpen(true);
   };
+
+  const address = getAddress();
+
+  const [balance, setBalance] = useState(0);
+
+  const getBalance = async () => {
+    try {
+      let res = await getNativeBalance({
+        chain: 97,
+        address: address,
+      });
+
+      let balance_ = new BigNumer(res?.toString(10)).div(10 ** 18).toString();
+
+      setBalance(balance_);
+    } catch (err) {
+      console.log(err);
+      setBalance(0);
+    }
+  };
+
+  useEffect(() => {
+    getBalance();
+
+    let interval = setInterval(() => {
+      getBalance();
+    }, 7_000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [address]);
 
   return (
     <>
@@ -29,33 +67,38 @@ function DonateModal() {
         <div className="fieldDetailModal">
           <div className="fieldDetailTitle">Donate</div>
 
-         <div className="donateInputWrapper">
-          <div className="donateInputHeader">
-            <span>
-              Amount
-            </span>
-            <span>
-              Balance: <b>14.99 BNB</b>
-            </span>
+          <div className="donateInputWrapper">
+            <div className="donateInputHeader">
+              <span>Amount</span>
+
+              {isConnected && (
+                <span>
+                  Balance:{" "}
+                  <b>
+                    {balance
+                      ? parseFloat(balance)?.toLocaleString("en-US")
+                      : "-"}{" "}
+                    BNB
+                  </b>
+                </span>
+              )}
+            </div>
+
+            <div className="donateInput">
+              <input
+                type="number"
+                placeholder="0.01"
+                onKeyPress={(event) => {
+                  if (!/[0-9+.]/.test(event.key)) {
+                    event.preventDefault();
+                  }
+                }}
+              />
+              <span>BNB</span>
+            </div>
           </div>
 
-          <div className="donateInput">
-            <input 
-              type="number"
-              placeholder="0.01"
-              onKeyPress={(event) => {
-                if (!/[0-9+.]/.test(event.key)) {
-                  event.preventDefault();
-                }
-              }}
-            />
-            <span>BNB</span>
-          </div>
-         </div>
-
-          <button className="subBtn">
-            Submit
-          </button>
+          <button className="subBtn">Submit</button>
         </div>
       </Modal>
     </>
