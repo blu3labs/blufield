34bb3b16e25db642d976e0b102f583da56eed3b4
Mehmet Çığırgan <mehmet.cigirgan@ogr.deu.edu.tr>
2023-11-12 02:57:20 +0300

    frontend helmet remove

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import Video from "@/assets/video.jpeg";
 import Text from "@/assets/text.jpeg";
 import Audio from "@/assets/audio.jpeg";
@@ -24,40 +23,40 @@ function Home() {
     },
   ];
   return (
-    <>
-      <Helmet>
-        <title>Home | Blufield</title>
-      </Helmet>
-
-      <div className="homeWrapper">
-        <div className="homeHeader">
-         Blufield
-        </div>
-        {/* <div className="homeTitle">Lorem, ipsum dolor.</div> */}
-        <div className="homeSubtitle">
-          Lorem ipsum dolor sit amet consectetur adipisicing.
-        </div>
-
-        <div className="homeItems">
-          {items.map((item, index) => (
-            <div className="homeItem" key={index}>
-              <div className="homeItemTitle">{item.title}</div>
-              <img src={item.image} alt="" draggable={false} />
-              <div className="homeItemDesc">{item.desc}</div>
-            </div>
-          ))}
-        </div>
-
-        <div className="homeSocials">
-          <a href="https://twitter.com/blu3labs" target="_blank" rel="noopener noreferrer">
-            Twitter 
-          </a>
-          <a href="mailto:contact@blu3.dev" target="_blank" rel="noopener noreferrer">
-            Email
-          </a>
-        </div>
+    <div className="homeWrapper">
+      <div className="homeHeader">Blufield</div>
+      {/* <div className="homeTitle">Lorem, ipsum dolor.</div> */}
+      <div className="homeSubtitle">
+        Lorem ipsum dolor sit amet consectetur adipisicing.
       </div>
-    </>
+
+      <div className="homeItems">
+        {items.map((item, index) => (
+          <div className="homeItem" key={index}>
+            <div className="homeItemTitle">{item.title}</div>
+            <img src={item.image} alt="" draggable={false} />
+            <div className="homeItemDesc">{item.desc}</div>
+          </div>
+        ))}
+      </div>
+
+      <div className="homeSocials">
+        <a
+          href="https://twitter.com/blu3labs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <a
+          href="mailto:contact@blu3.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Email
+        </a>
+      </div>
+    </div>
   );
 }
 
