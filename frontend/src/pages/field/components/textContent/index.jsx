84881e0a129cb83dd5520f9f8d0c413b55be2766
Mehmet Çığırgan <mehmet.cigirgan@ogr.deu.edu.tr>
2023-11-11 14:32:05 +0300

    field page v5

--- frontend/src/pages/field/components/textContent/index.jsx
+++ frontend/src/pages/field/components/textContent/index.jsx
@@ -5,17 +5,16 @@ import "./index.css"
 function TextContent({data}) {
   return (
     <Link to="/text/2" className="fieldTextContent">
-    <div className="fieldTextContentHeader">
-      <span>Text</span>
-      <span>|</span>
-      <span>16 mins ago</span>
-    </div>
+   
     <img src={data.banner} alt="banner" draggable="false" />
     <div className="fieldTextContentTitle">
       Lorem ipsum dolor sit amet.
     </div>
     <div className="fieldTextContentShortText">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae hic vero voluptate?
+    </div>
+     <div className="fieldTextContentDate">
+      11 November, 23:40 
     </div>
   </Link>
   )
