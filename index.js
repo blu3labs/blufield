9ba03ef80f3b67f82d81d111a917aa5229a2c20b
batuhan soylu <bthnsoylu35@gmail.com>
2023-11-11 14:20:24 +0300

    get users and user method added

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 require("dotenv").config();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
-const {client} = require("./config/client");
-const {createBucket} = require("./config/createBucket");
+const { client } = require("./config/client");
+const { createBucket } = require("./config/createBucket");
 const app = express();
 app.use(bodyParser.json());
 app.use(
@@ -15,11 +15,11 @@ app.use(
 );
 app.use(cors());
 //services
-const {pushImage, createUser} = require("./services/userServices");
+const { pushImage, createUser, getUser,getAllUser } = require("./services/userServices");
 
 async function checkBucket() {
   try {
-    if(!process.env.BUCKET_NAME){
+    if (!process.env.BUCKET_NAME) {
       await createBucket(client);
     }
   } catch (error) {
@@ -27,27 +27,33 @@ async function checkBucket() {
   }
 }
 
-
 checkBucket();
-
-
-
 
 app.post("/img", upload.single("file"), async (req, res) => {
   const file = req.file;
   console.log(file);
-  const response = await pushImage(file,client);
+  const response = await pushImage(file, client);
   res.json(response);
 });
 
+app.post("/user", async (req, res) => {
+  const body = req.body;
+  const response = await createUser(body, client);
+  console.log(response);
+  res.status(response.status).json(response);
+});
+
+app.get("/user", async (req, res) => {
+  const response = await getAllUser(client);
+  res.status(response.status).json(response);
+});
 
 
-
-
-
-
-
-
+app.get("/user/:name", async (req, res) => {
+  const name = req.params.name;
+  const response = await getUser(name, client);
+  res.status(response.status).json(response);
+});
 
 
 const port = process.env.PORT || API_PORT;
--- services/userServices.js
+++ services/userServices.js
@@ -2,12 +2,13 @@ const { generateString } = require("../utils/generateString");
 const { getAllSps } = require("../utils/getSps");
 const { broadcast } = require("../utils/broadcast");
 const fs = require("fs");
-
+const axios = require("axios");
 const { getCheckSums } = require("@bnb-chain/greenfiled-file-handle");
 
 async function pushImage(file, client) {
   try {
     // save file use fs
+    const timeStamps = new Date().getTime();
     const filedata = fs.readFileSync(file.path);
     const { expectCheckSums, contentLength } = await getCheckSums(filedata);
     const imageTx = await client.object.createObject(
@@ -17,7 +18,7 @@ async function pushImage(file, client) {
         creator: process.env.WALLET_ADDRESS,
         expectCheckSums: JSON.parse(expectCheckSums),
         fileType: file.mimetype.split("/")[1],
-        objectName: file.originalname,
+        objectName: "images/" + timeStamps + file.originalname,
         redundancyType: "REDUNDANCY_EC_TYPE",
         visibility: "VISIBILITY_TYPE_PUBLIC_READ",
       },
@@ -32,11 +33,11 @@ async function pushImage(file, client) {
     const txHash = await imageTx.broadcast({
       ...broadcast,
     });
-
+    console.log(txHash);
     const uploadRes = await client.object.uploadObject(
       {
         bucketName: "users",
-        objectName: file.originalname,
+        objectName: "images/" + timeStamps + file.originalname,
         body: filedata,
         txnHash: txHash.transactionHash,
       },
@@ -48,6 +49,7 @@ async function pushImage(file, client) {
         address: process.env.WALLET_ADDRESS,
       }
     );
+    console.log(uploadRes);
     const allSps = await getAllSps(client);
     fs.unlinkSync(file.path);
     return {
@@ -61,21 +63,131 @@ async function pushImage(file, client) {
   }
 }
 
-async function createUser(
-  name,
-  address,
-  about,
-  cover_image,
-  profile_image,
-  bucket_id,
-  bucket_name,
-  createdDate,
-  accentColor,
-  socail_media,
-  montly_price
-) {}
+async function createUser(body, client) {
+  try {
+    console.log(body);
+    const writeFile = fs.writeFileSync("user.json", JSON.stringify(body));
+
+    const filedata = fs.readFileSync("user.json");
+    console.log(filedata);
+
+    console.log("check sum started ismail....");
+    const { expectCheckSums, contentLength } = await getCheckSums(filedata);
+    console.log(expectCheckSums, contentLength);
+    const userTx = await client.object.createObject(
+      {
+        bucketName: "users",
+        contentLength: contentLength,
+        creator: process.env.WALLET_ADDRESS,
+        expectCheckSums: JSON.parse(expectCheckSums),
+        fileType: "json",
+        objectName: body.name + ".json",
+        redundancyType: "REDUNDANCY_EC_TYPE",
+        visibility: "VISIBILITY_TYPE_PUBLIC_READ",
+      },
+      {
+        type: "ECDSA",
+        privateKey: "0x" + process.env.PRIVATE_KEY,
+        domain: generateString(10),
+        seed: generateString(10),
+        address: process.env.WALLET_ADDRESS,
+      }
+    );
+    console.log(userTx);
+    const txHash = await userTx.broadcast({
+      ...broadcast,
+    });
+    console.log(txHash);
+
+    const uploadRes = await client.object.uploadObject(
+      {
+        bucketName: "users",
+        objectName: body.name + ".json",
+        body: filedata,
+        txnHash: txHash.transactionHash,
+      },
+      {
+        type: "ECDSA",
+        privateKey: "0x" + process.env.PRIVATE_KEY,
+        domain: generateString(10),
+        seed: generateString(10),
+        address: process.env.WALLET_ADDRESS,
+      }
+    );
+    console.log(uploadRes);
+    fs.unlinkSync("user.json");
+    return {
+      message: "success",
+      status: 200,
+    };
+  } catch (error) {
+    console.log(error, "error");
+
+    return {
+      message: error,
+      status: 500,
+    };
+  }
+}
+
+async function getAllUser(client) {
+  try {
+    const sps = await getAllSps(client);
+    console.log(sps);
+    const getAllBucketIds = await client.object.listObjects({
+      bucketName: "users",
+      endpoint: sps[0].endpoint,
+    });
+    let objects = [
+      ...getAllBucketIds.body.GfSpListObjectsByBucketNameResponse.Objects,
+    ];
+    objects = objects.filter((v) => v.ObjectInfo.ObjectName.includes(".json"));
+    let users = [];
+    for (let i of objects) {
+      try {
+        const { data: response } = await axios.get(
+          sps[0].endpoint + "/view/users/" + i.ObjectInfo.ObjectName
+        );
+        users.push(response);
+      } catch (error) {
+        console.log("errr");
+      }
+    }
+    return {
+      data: users,
+      status: 200,
+    };
+  } catch (error) {
+    console.log("err");
+    return {
+      error: error,
+      status: 500,
+    };
+  }
+}
+
+async function getUser(name, client) {
+  try {
+    const sps = await getAllSps(client);
+    const { data: response } = await axios.get(
+      sps[0].endpoint + "/view/users/" + name + ".json"
+    );
+    return {
+      data: response,
+      status: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      error: error,
+      status: 500,
+    };
+  }
+}
 
 module.exports = {
   createUser,
   pushImage,
+  getUser,
+  getAllUser,
 };
