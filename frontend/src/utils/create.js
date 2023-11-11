8478b9c8966c47152c1cf18e1716de807fc6d924
CryptoS0s <devorso@users.noreply.github.com>
2023-11-11 20:49:25 +0300

    create text

---
+++ frontend/src/utils/create.js
@@ -0,0 +1,61 @@
+import { getOffchainAuthKeys } from "./auth";
+import { client } from "./client";
+
+
+
+
+let metadataFormat = {
+    banner:"",
+    title:"",
+    short_text:"",
+    text:"",
+    visibility:""
+}
+export const CreateData = async (bucketName, address, signer, switchNetworkAsync,data, type) => {
+
+    if (type === "text") {
+
+        // create object
+
+        const client = client
+        const auth = getOffchainAuthKeys(address, signer, switchNetworkAsync)
+        const filedata = Buffer.from(JSON.stringify(data).toString("utf8"));
+        const { expectCheckSums, contentLength } = await getCheckSums(filedata);
+        const signingData = {
+            type: "EDDSA",
+            seed: auth.seedString,
+            domain: window.location.origin,
+            address,
+          };
+        
+        const userTx = await client.object.createObject(
+          {
+            bucketName: bucketName,
+            contentLength: contentLength,
+            creator: address,
+            expectCheckSums: JSON.parse(expectCheckSums),
+            fileType: "json",
+            objectName: body.name + ".json",
+            redundancyType: "REDUNDANCY_EC_TYPE",
+            visibility: data.visibility,
+          },
+         signingData
+        );
+        const txHash = await userTx.broadcast({
+          ...broadcast,
+        });
+    
+        const uploadRes = await client.object.uploadObject(
+          {
+            bucketName: bucketName,
+            objectName: body.name + ".json",
+            body: filedata,
+            txnHash: txHash.transactionHash,
+          },
+          signingData
+        );
+
+        console.log(uploadRes, " upload result")
+        // upload object
+    }
+}
