559cbc59096595f2a11fe3e271b803cc037c2889
Ahmet <96299039+ahmtguler@users.noreply.github.com>
2023-11-11 17:22:12 +0300

    fix

--- blufield-contracts/contracts/Blufield.sol
+++ blufield-contracts/contracts/Blufield.sol
@@ -3,19 +3,19 @@
 pragma solidity 0.8.20;
 
 import {IERC721NonTransferable} from "@bnb-chain/greenfield-contracts/contracts/interface/IERC721NonTransferable.sol";
+import {IERC1155NonTransferable} from "@bnb-chain/greenfield-contracts/contracts/interface/IERC1155NonTransferable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
-import {GroupApp, GroupStorage, CmnStorage} from "@bnb-chain/greenfield-contracts-sdk/GroupApp.sol";
+import {GroupApp, GroupStorage, IGroupHub, CmnStorage} from "@bnb-chain/greenfield-contracts-sdk/GroupApp.sol";
 
 contract Blufield is GroupApp, ReentrancyGuardUpgradeable {
     IERC721NonTransferable public GROUP_TOKEN;
-    uint256 public creationFee;
+    IERC1155NonTransferable public MEMBER_TOKEN;
 
     mapping(address => uint256) public fieldIdOfUser;
     mapping(uint256 => string) public fieldIdToName;
     mapping(string => uint256) public fieldNameToId;
     mapping(uint256 => uint256) public subscriptionFee;
     mapping(address => mapping(uint256 => uint64)) public expirationOfUserSubscription;
-    mapping(address => uint256) public pendingWithdrawals;
 
     function initialize(
         address _groupHub,
@@ -24,6 +24,7 @@ contract Blufield is GroupApp, ReentrancyGuardUpgradeable {
         uint8 _failureHandleStrategy
     ) public initializer {
         GROUP_TOKEN = IERC721NonTransferable(CmnStorage(_groupHub).ERC721Token());
+        MEMBER_TOKEN = IERC1155NonTransferable(GroupStorage(_groupHub).ERC1155Token());
         __base_app_init_unchained(_crossChain, _callbackGasLimit, _failureHandleStrategy);
         __group_app_init_unchained(_groupHub);
     }
@@ -46,29 +47,51 @@ contract Blufield is GroupApp, ReentrancyGuardUpgradeable {
         require(owner != address(0), "Blufield: cannot subscribe to own field");
         
         uint256 fee = subscriptionFee[groupId];
-        require(msg.value >= fee, "Blufield: insufficient payment");
-        pendingWithdrawals[owner] += fee;
+        uint256 totalFee = _getTotalFee();
+        require(msg.value >= fee + totalFee, "Blufield: insufficient payment");
         
-        uint64 expiration = expirationOfUserSubscription[msg.sender][groupId];
+        uint64 expiration;
         GroupStorage.UpdateGroupOpType opType;
-        if (expiration == 0) {
-            expirationOfUserSubscription[msg.sender][groupId] = uint64(block.timestamp + 30 days);
+        uint256 balance = MEMBER_TOKEN.balanceOf(msg.sender, groupId);
+        if (balance == 0) {
+            expiration = uint64(block.timestamp + 30 days);
             opType = GroupStorage.UpdateGroupOpType.AddMembers;
         } else {
-            expirationOfUserSubscription[msg.sender][groupId] = expiration + 30 days;
+            if (expirationOfUserSubscription[msg.sender][groupId] > block.timestamp) {
+                expiration = uint64(expirationOfUserSubscription[msg.sender][groupId] + 30 days);
+            } else {
+                expiration = uint64(block.timestamp + 30 days);
+            }
             opType = GroupStorage.UpdateGroupOpType.RenewMembers;
         }
         address[] memory members = new address[](1);
         uint64[] memory expirations = new uint64[](1);
         members[0] = msg.sender;
         expirations[0] = expiration;
-        _updateGroup(owner, groupId, opType, members, expirations);
+
+        _updateGroupWithValue(owner, groupId, opType, members, expirations, msg.value - fee);
+        payable(owner).transfer(fee);
     }
 
-    function withdraw() external nonReentrant {
-        uint256 amount = pendingWithdrawals[msg.sender];
-        require(amount > 0, "Blufield: no pending withdrawals");
-        pendingWithdrawals[msg.sender] = 0;
-        payable(msg.sender).transfer(amount);
+    function _updateGroupWithValue(
+        address _owner,
+        uint256 _tokenId,
+        GroupStorage.UpdateGroupOpType _opType,
+        address[] memory _members,
+        uint64[] memory _expiration,
+        uint256 _value
+    ) private {
+        GroupStorage.UpdateGroupSynPackage memory updatePkg = GroupStorage.UpdateGroupSynPackage({
+            operator: _owner,
+            id: _tokenId,
+            opType: _opType,
+            members: _members,
+            extraData: "",
+            memberExpiration: _expiration
+        });
+
+        uint256 totalFee = _getTotalFee();
+        require(msg.value >= totalFee, string.concat("GroupApp: ", ERROR_INSUFFICIENT_VALUE));
+        IGroupHub(groupHub).updateGroup{value: _value}(updatePkg);
     }
 }
